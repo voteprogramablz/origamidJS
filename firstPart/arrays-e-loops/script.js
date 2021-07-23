7692528e14173b093ffd0b72593bda6961d63fc7
André Rodrigues de Melo <78188012+voteprogramablz@users.noreply.github.com>
2021-07-23 19:36:03 -0300

    Add files via upload

---
+++ firstPart/arrays-e-loops/script.js
@@ -0,0 +1,29 @@
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002]
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+for (let i = 0 ; i < brasilCampeao.length ; i++) {
+    console.log(`O brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+for( let i = 0; i < frutas.length ; i ++) {
+    console.log(frutas[i])
+    if (frutas[i] === 'Pera'){
+        break
+    }
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var frutasLastItem = frutas[frutas.length - 1]
+
+console.log(`\n ${frutasLastItem}`)
+console.log('\n' + frutas);
